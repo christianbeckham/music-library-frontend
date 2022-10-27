536ef24b855e5c700dda0284b8f7faa25ada16aa
Christian Beckham <urenacb22@gmail.com>
2022-10-26 19:40:59 -0500

    feat: added icon to button and added functionality to confirm delete with modal

--- src/components/DeleteSongButton/DeleteSongButton.jsx
+++ src/components/DeleteSongButton/DeleteSongButton.jsx
@@ -1,16 +1,55 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 const DeleteSongButton = (props) => {
-	const { songId, deleteSong } = props;
+	const { song, deleteSong } = props;
+	const [showAlertModal, setShowAlertModal] = useState(false);
+	const toggleAlert = () => setShowAlertModal(!showAlertModal);
 
-	const handleOnClick = () => deleteSong(songId);
+	const handleOnClickDelete = () => {
+		deleteSong(song.id);
+		setShowAlertModal(false);
+	};
+	const handleOnClickCancel = () => {
+		setShowAlertModal(false);
+	};
 
 	return (
 		<div>
-			<Button variant="danger" onClick={handleOnClick}>
-				Delete
+			<Button className="mx-1" variant="danger" size="sm" onClick={toggleAlert}>
+				<i className="bi-x-lg" role="img" aria-label="delete icon"></i>
 			</Button>
+			{showAlertModal && (
+				<Modal
+					contentClassName="modal-bg-dark"
+					show={showAlertModal}
+					onHide={handleOnClickCancel}
+					backdrop="static"
+				>
+					<Modal.Header closeButton closeVariant="white">
+						<Modal.Title>Delete Confirmation</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<p className="fs-5">Are you sure you want to delete this song?</p>
+						<p className="alert alert-danger text-center">
+							<span className="text-uppercase">{song.title}</span>
+							<span> by </span>
+							<span className="text-uppercase">{song.artist}</span>
+						</p>
+						<p className="fw-lighter fst-italic">
+							*This action is permanent and cannot be undone.
+						</p>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="danger" onClick={handleOnClickDelete}>
+							Delete
+						</Button>
+						<Button variant="secondary" onClick={handleOnClickCancel}>
+							Cancel
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			)}
 		</div>
 	);
 };
