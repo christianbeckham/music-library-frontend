8196f8bf4848187fb2b7b9cc20addc6a98b10783
Christian Beckham <urenacb22@gmail.com>
2022-10-31 16:48:35 -0500

    created new custom command to create song

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("createSong", (songObject) => {
+	cy.visit("http://localhost:3000");
+	cy.get('[data-test="add-song-btn"]').click();
+	cy.get('[data-test="title-input"]').type(songObject.title);
+	cy.get('[data-test="artist-input"]').type(songObject.artist);
+	cy.get('[data-test="album-input"]').type(songObject.album);
+	cy.get('[data-test="genre-input"]').type(songObject.genre);
+	cy.get('[data-test="date-input"]').type(songObject.release_date);
+	cy.get('[data-test="modalFormSaveBtn"]').click();
+});
